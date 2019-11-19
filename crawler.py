8d1107eba7639cc0f657cb928c81bfbe47e01655
Simon Ellmann <simon.ellmann@tum.de>
2019-11-19 17:42:02 +0100

    Simplify crawler

--- crawler.py
+++ crawler.py
@@ -2,9 +2,7 @@
 
 import os
 import django
-import re
 import requests
-import psycopg2
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mensa.settings")
 django.setup()
@@ -12,36 +10,34 @@ django.setup()
 from homometer.models import Stat
 
 
-API = 'http://graphite-kom.srv.lrz.de'
+def get_stats():
+    return requests.get("http://graphite-kom.srv.lrz.de/render/?from=-1hours&target=ap.ap*-?mg*.ssid.*&format=json").json()
 
 
-def db_update_stats(stats):
-    for k, v in stats:
+def update_stats(stats):
+    for k, v in stats.items():
         Stat.objects.update_or_create(
             ap=k,
             defaults={'current': v},
         )
 
 
-def get_stats():
-    return requests.get(API + "/render/?from=-1hours&target=ap.ap*-?mg*.ssid.*&format=json").json()
-
-
 def main():
     page = get_stats()
 
-    stats = dict()
-    aps = list(map(lambda x: (x['target'].split('.')[1], x['datapoints'][-1][0] if x['datapoints'][-1][0] else x['datapoints'][-2][0] if x['datapoints'][-2][0] else 0), page))
+    aps = list(map(lambda x: (x['target'].split('.')[1],
+                              x['datapoints'][-1][0] if x['datapoints'][-1][0]
+                              else x['datapoints'][-2][0] if x['datapoints'][-2][0]
+                              else 0), page))
 
+    stats = dict()
     for (ap, current) in aps:
         if ap in stats:
             stats[ap] += current
         else:
             stats[ap] = current
 
-    stats = [(k, v) for k, v in stats.items()]
-
-    db_update_stats(stats)
+    update_stats(stats)
 
 
 if __name__ == "__main__":
