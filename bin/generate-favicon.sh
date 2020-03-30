eebe5ec1ee726e4cff4f68e06fb95a3921a39ec1
Tim Guggenmos <tim.guggenmos@me.com>
2020-03-30 18:17:07 +0200

    icon scalings + fix icon path + change to more recognizable icon

--- bin/generate-favicon.sh
+++ bin/generate-favicon.sh
@@ -2,9 +2,13 @@
 
 svgexport svg/icon.svg static/favicon-16.png 16:16
 svgexport svg/icon.svg static/favicon-32.png 32:32
-# svgexport svg/icon.svg static/favicon-128.png 128:128
-# svgexport svg/icon.svg static/favicon-152.png 152:152
-# svgexport svg/icon.svg static/favicon-167.png 167:167
-# svgexport svg/icon.svg static/favicon-180.png 180:180
+svgexport svg/icon.svg static/favicon-180.png 180:180
+
+svgexport svg/icon.svg static/favicon-72.png 72:72
+svgexport svg/icon.svg static/favicon-96.png 96:96
+svgexport svg/icon.svg static/favicon-128.png 128:128
+svgexport svg/icon.svg static/favicon-144.png 144:144
+svgexport svg/icon.svg static/favicon-152.png 152:152
 svgexport svg/icon.svg static/favicon-192.png 192:192
-# svgexport svg/icon.svg static/favicon-196.png 196:196
+svgexport svg/icon.svg static/favicon-384.png 384:384
+svgexport svg/icon.svg static/favicon-512.png 512:512
