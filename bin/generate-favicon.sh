333dac8b4ff77b41933f7b0ac41a01ac56960fb1
Tim Guggenmos <tim.guggenmos@me.com>
2020-03-30 18:45:07 +0200

    Enhance favicon and add web app manifest

---
+++ bin/generate-favicon.sh
@@ -0,0 +1,14 @@
+# npm install svgexport -g
+
+svgexport svg/icon.svg static/favicon-16.png 16:16
+svgexport svg/icon.svg static/favicon-32.png 32:32
+svgexport svg/icon.svg static/favicon-180.png 180:180
+
+svgexport svg/icon.svg static/favicon-72.png 72:72
+svgexport svg/icon.svg static/favicon-96.png 96:96
+svgexport svg/icon.svg static/favicon-128.png 128:128
+svgexport svg/icon.svg static/favicon-144.png 144:144
+svgexport svg/icon.svg static/favicon-152.png 152:152
+svgexport svg/icon.svg static/favicon-192.png 192:192
+svgexport svg/icon.svg static/favicon-384.png 384:384
+svgexport svg/icon.svg static/favicon-512.png 512:512
