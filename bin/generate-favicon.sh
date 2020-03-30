7f9041243c00359620abf6b62c996ec79ba0e619
Tim Guggenmos <tim.guggenmos@me.com>
2020-03-30 17:57:27 +0200

    Resolve most change requests

--- bin/generate-favicon.sh
+++ bin/generate-favicon.sh
@@ -5,6 +5,6 @@ svgexport svg/icon.svg static/favicon-32.png 32:32
 # svgexport svg/icon.svg static/favicon-128.png 128:128
 # svgexport svg/icon.svg static/favicon-152.png 152:152
 # svgexport svg/icon.svg static/favicon-167.png 167:167
-svgexport svg/icon.svg static/favicon-180.png 180:180
-# svgexport svg/icon.svg static/favicon-192.png 192:192
+# svgexport svg/icon.svg static/favicon-180.png 180:180
+svgexport svg/icon.svg static/favicon-192.png 192:192
 # svgexport svg/icon.svg static/favicon-196.png 196:196
