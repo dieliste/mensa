c5cdda29a8b7815c203ed7bc682486aead8c4524
Simon Ellmann <simon.ellmann@tum.de>
2021-10-11 12:59:38 +0200

    Update homometer to Django 3.2

--- mensa/settings.py
+++ mensa/settings.py
@@ -122,6 +122,12 @@ LANGUAGES = [
     ('en', _('English')),
 ]
 
+
+# Type of auto-created primary keys
+# https://docs.djangoproject.com/en/3.2/releases/3.2/#customizing-type-of-auto-created-primary-keys
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
