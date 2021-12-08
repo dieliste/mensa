7e8e6a62a6e4012306198d21ba7e0b3eb0e5b935
Philipp <Philipp000@users.noreply.github.com>
2021-12-08 12:36:50 +0100

    Add API endpoint returning current and percent (#8)

--- homometer/views.py
+++ homometer/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.db.models import Sum
+from django.http import JsonResponse
 from django.views.decorators.clickjacking import xframe_options_exempt
 from math import floor
 
 from .models import Stat
 
-
-def index(request, minimal=False):
+def getCurrentAndPercent():
     # It would be nice if the aggregate function could return a default value when
     # there are no rows: https://code.djangoproject.com/ticket/10929
     current = Stat.objects.aggregate(Sum('current'))['current__sum'] or 0
-
     # clip current to range [0, 1500] and convert to percent
     percent = (sorted([0, current, 1500])[1] / 1500) * 100
+    return current, percent
+
+
+def index(request, minimal=False):
+    current, percent = getCurrentAndPercent()
 
     # determine homometer color
     colors = ['#2dde57', '#2dde57', '#2dde57', '#f3ad4e', '#f3ad4e', '#f3ad4e', '#f3ad4e', '#ed0009', '#ed0009', '#ed0009', '#ed0009']
@@ -31,3 +35,7 @@ def index(request, minimal=False):
 @xframe_options_exempt
 def index_minimal(request):
     return index(request, minimal=True)
+
+def api(request):
+    current, percent = getCurrentAndPercent()
+    return JsonResponse({"current": current, "percent": percent})
